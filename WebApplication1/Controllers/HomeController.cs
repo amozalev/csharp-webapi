57e581d5ee66ea5eeae67e134cc2d9dc30e4beaf
amozalev <mozalev.alexey@gmail.com>
2019-07-23 09:33:03 +0300

    Custom response class is implemented. JSON is returned in controller.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,38 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
- 
+using System.Web.Http.Results;
+using System.Web.Mvc;
+//using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
 namespace WebApplication1.Controllers
 {
+    public class ResponseClass : IHttpActionResult
+    {
+        string _value;
+        HttpRequestMessage _request;
+
+        public ResponseClass(string value, HttpRequestMessage request)
+        {
+            _value = value;
+            _request = request;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(_value),
+                RequestMessage = _request
+            };
+            return Task.FromResult(response);
+        }
+    }
+
     public class ValuesController : ApiController
     {
+        UserRepository repo = new UserRepository();
+
         // GET api/values
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var users = repo.GetUsers();
+            return new ResponseClass(JsonConvert.SerializeObject(users), Request);
         }
- 
+
         // GET api/values/5
-        public string Get(int id)
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            var user = repo.Get(id);
+            return new ResponseClass(JsonConvert.SerializeObject(user), Request);
         }
- 
+
         // POST api/values
-        public void Post([FromBody]string value)
-        {}
- 
+        public void Post([FromBody] string value)
+        {
+        }
+
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {}
- 
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
         // DELETE api/values/5
         public void Delete(int id)
-        {}
+        {
+        }
     }
 }
--- WebApplication1/Models/UserRepository.cs
+++ WebApplication1/Models/UserRepository.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Results;
 using Dapper;
 using Newtonsoft.Json;
 using System.Web.Script.Serialization;
@@ -18,24 +20,27 @@ namespace WebApplication1.Models
         string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
 
+        [HttpGet]
         public IEnumerable<User> GetUsers()
         {
             List<User> users = new List<User>();
             using (IDbConnection db = new SQLiteConnection(connectionString))
             {
                 users = db.Query<User>("SELECT * FROM Users").AsList();
-                Console.WriteLine(users);
+//                Console.WriteLine("users: {0}", JsonConvert.SerializeObject(users));
             }
 
             return users;
+//            return Json(users);
+//                return new JavaScriptSerializer().Serialize(users);
         }
 
         public User Get(int id)
         {
             User user = null;
-            using (IDbConnection db = new SqlConnection(connectionString))
+            using (IDbConnection db = new SQLiteConnection(connectionString))
             {
-                user = db.Query<User>("SELECT * FROM Users WHERE Id = @id", new {id}).FirstOrDefault();
+                user = db.Query<User>("SELECT * FROM Users WHERE id = @id", new {id}).FirstOrDefault();
             }
 
             return user;
@@ -43,7 +48,7 @@ namespace WebApplication1.Models
 
         public User Create(User user)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
+            using (IDbConnection db = new SQLiteConnection(connectionString))
             {
                 var sqlQuery =
                     "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
@@ -56,7 +61,7 @@ namespace WebApplication1.Models
 
         public void Update(User user)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
+            using (IDbConnection db = new SQLiteConnection(connectionString))
             {
                 var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
                 db.Execute(sqlQuery, user);
@@ -65,7 +70,7 @@ namespace WebApplication1.Models
 
         public void Delete(int id)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
+            using (IDbConnection db = new SQLiteConnection(connectionString))
             {
                 var sqlQuery = "DELETE FROM Users WHERE Id = @id";
                 db.Execute(sqlQuery, new {id});
