a7dca53265075ea0e288684441744e4162493e03
amozalev <mozalev.alexey@gmail.com>
2019-07-28 20:24:10 +0300

    State is corrected and added.

--- WebApplication1/client/src/UserList.tsx
+++ WebApplication1/client/src/UserList.tsx
@@ -9,26 +9,18 @@ interface IState {
     users: User[];
 }
 
-class UserList extends React.Component {
-    // state: IState;
-    state = {
-        users: []
-    };
-
-    // userList: [];
+class UserList extends React.Component<IState> {
+    state: IState;
 
     constructor(props: UserListProps) {
         super(props);
-        // this.state = []
-        // this.userList = null;
+        this.state = {'users': []};
     }
-
 
     componentDidMount() {
         fetch('http://localhost:5000/api/users')
             .then(res => res.json())
             .then((data) => {
-                console.log('data: ', data);
                 this.setState({users: data});
             })
             .catch(console.log)
@@ -43,8 +35,8 @@ class UserList extends React.Component {
             {lst}
         </ul>)
     }
-    
-    render(){
+
+    render() {
         return this.setUsers()
     }
 }
